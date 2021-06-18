d5536b264baf618865ea35feef7d9d220cfd9145
sakumashirayuki <2909549511@qq.com>
2021-06-19 03:05:48 +0800

    config the router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import { About, Auth, Cart, Checkout, Error, Home, PrivateRoute, Products, SingleProduct} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route path="/products/:id" children={SingleProduct}/>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App
