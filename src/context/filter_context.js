9bb6f3302857528b97085a9e23e3f6674035412b
sakumashirayuki <2909549511@qq.com>
2021-06-24 17:01:31 +0800

    finish sort functioning

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,7 +15,8 @@ import { useProductsContext } from './products_context'
 const initialState = {
   filteredProduct: [],
   allProducts: [],
-  gridView: false
+  gridView: false,
+  sort: 'price-lowest'
 }
 
 const FilterContext = React.createContext()
@@ -23,11 +24,31 @@ const FilterContext = React.createContext()
 export const FilterProvider = ({ children }) => {
   const { products } = useProductsContext();
   const [state, dispatch] = useReducer(reducer, initialState);
+  
+  const setGridView = () => {
+    dispatch({type: SET_GRIDVIEW});
+  };
+
+  const setListView = () => {
+    dispatch({type: SET_LISTVIEW});
+  };
+
+  const updateSort = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    dispatch({type: UPDATE_SORT, payload: value});
+  }
+
   useEffect(()=>{
     dispatch({type: LOAD_PRODUCTS, payload: products});
   }, [products]);
+
+  useEffect(()=>{
+    dispatch({type: SORT_PRODUCTS});
+  }, [products, state.sort]);
+
   return (
-    <FilterContext.Provider value={{...state}}>
+    <FilterContext.Provider value={{...state, setGridView, setListView, updateSort}}>
       {children}
     </FilterContext.Provider>
   )
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -1,26 +1,60 @@
 import {
-    LOAD_PRODUCTS,
-    SET_LISTVIEW,
-    SET_GRIDVIEW,
-    UPDATE_SORT,
-    SORT_PRODUCTS,
-    UPDATE_FILTERS,
-    FILTER_PRODUCTS,
-    CLEAR_FILTERS,
+  LOAD_PRODUCTS,
+  SET_LISTVIEW,
+  SET_GRIDVIEW,
+  UPDATE_SORT,
+  SORT_PRODUCTS,
+  UPDATE_FILTERS,
+  FILTER_PRODUCTS,
+  CLEAR_FILTERS,
 } from "../actions";
 
 const filter_reducer = (state, action) => {
-    switch (action.type) {
-        case LOAD_PRODUCTS:
-            return {
-                ...state,
-                allProducts: [...action.payload],
-                filteredProduct: [...action.payload],
-            };
-
+  switch (action.type) {
+    case LOAD_PRODUCTS:
+      return {
+        ...state,
+        allProducts: [...action.payload],
+        filteredProduct: [...action.payload],
+      };
+    case SET_GRIDVIEW:
+      return {
+        ...state,
+        gridView: true,
+      };
+    case SET_LISTVIEW:
+      return {
+        ...state,
+        gridView: false,
+      };
+    case UPDATE_SORT:
+      return {
+        ...state,
+        sort: action.payload,
+      };
+    case SORT_PRODUCTS:
+      const { sort, filteredProduct } = state;
+      let tempProducts = [...filteredProduct];
+      switch (sort) {
+        case "price-lowest":
+          tempProducts.sort((a, b) => a.price - b.price);
+          break;
+        case "price-highest":
+          tempProducts.sort((a, b) => b.price - a.price);
+          break;
+        case "name-a":
+          tempProducts.sort((a, b) => a.name.localeCompare(b.name));
+          break;
+        case "name-z":
+          tempProducts.sort((a, b) => b.name.localeCompare(a.name));
+          break;
         default:
-            throw new Error(`No Matching "${action.type}" - action type`);
-    }
+          break;
+      }
+      return { ...state, filteredProduct: tempProducts };
+    default:
+      throw new Error(`No Matching "${action.type}" - action type`);
+  }
 };
 
 export default filter_reducer;
